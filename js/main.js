c6e871998a3a73c08951ffd6b563f8056b02de06
Amy Baker <amyraebaker@gmail.com>
2022-06-16 01:00:34 -0700

    Create function to calculate expression

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
+//add listeners to buttons to collect user input
 const buttons = document.querySelectorAll('.key')
 buttons.forEach(button => button.addEventListener('click', selectElement))
 
+//store button clicks in string and variables to create expression
 let input = ''
 let firstNum, secondNum, symbol
 function selectElement(e) {
@@ -11,8 +13,22 @@ function selectElement(e) {
     } else if (e.target.innerText === '=') {
         secondNum = input
         input = ''
+        console.log(calculate(firstNum, secondNum, symbol))
     } else {
         input += e.target.innerText
     }
     return firstNum, secondNum, symbol
+}
+
+//evaluate expression once user has entered both numbers and operation and pressed equals
+function calculate(num1, num2, operation) {
+    if (operation === '+') {
+        return +num1 + +num2
+    } else if (operation === '-') {
+        return +num1 - +num2
+    } else if (operation === 'x') {
+        return +num1 * +num2
+    } else if (operation === '/') {
+        return +num1 / +num2
+    }
 }
