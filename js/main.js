19c6655a602a05322e362a1857581c9983a4e5d9
Amy Baker <amyraebaker@gmail.com>
2022-06-17 14:44:01 -0700

    Round results to 10 decimal places

--- js/main.js
+++ js/main.js
@@ -51,7 +51,7 @@ function operate(num1, num2, operation) {
 }
 
 //Operation Functions
-const add = (a, b) => a + b
-const subtract = (a, b) => a - b
-const multiply = (a, b) => a * b
-const divide = (a, b) => a / b
+const add = (a, b) => (a + b).toFixed(10)
+const subtract = (a, b) => (a - b).toFixed(10)
+const multiply = (a, b) => (a * b).toFixed(10)
+const divide = (a, b) => (a / b).toFixed(10)
