0cb71e249cba506268614f46dfee299d94343eed
Amy Baker <amyraebaker@gmail.com>
2022-06-20 23:09:01 -0700

    Add function to calculate exponent and factorial

--- js/main.js
+++ js/main.js
@@ -9,18 +9,24 @@ let input = ''
 let firstNum, secondNum, symbol
 function selectElement(e) {
     let pressed = e.target.innerText
-    if ('+-x/'.includes(pressed) && !symbol) {
+    if ('+-x/^'.includes(pressed) && !symbol) {
         displayInput.innerText += pressed
         firstNum = input
         input = ''
         symbol = pressed
-    } else if ('+-x/'.includes(pressed) && symbol) {
+    } else if ('+-x/^'.includes(pressed) && symbol) {
         displayInput.innerText += pressed
         secondNum = input
         firstNum = operate(firstNum, secondNum, symbol)
         symbol = pressed
         secondNum = ''
         input = ''
+    } else if (pressed === '!') {
+        displayInput.innerText += pressed
+        firstNum = input
+        input = ''
+        symbol = pressed
+        operate(firstNum, '', symbol)
     } else if (pressed === '=') {
         if (firstNum && symbol && input) {
             displayInput.innerText += pressed
@@ -49,6 +55,13 @@ function operate(num1, num2, operation) {
     } else if (operation === '/') {
         displayResult.innerText = divide(num1, num2)
         return divide(num1, num2)
+    } else if (operation === '^') {
+        displayResult.innerText = exponent(num1, num2)
+        return exponent(num1, num2)
+    } else if (operation === '!') {
+        displayResult.innerText = factorial(+num1)
+        console.log(factorial(+num1))
+        return factorial(num1)
     }
 }
 
@@ -57,6 +70,8 @@ const add = (a, b) => (a + b) % 1 === 0 ? a + b : (a + b).toFixed(10)
 const subtract = (a, b) => (a - b) % 1 === 0 ? a - b : (a - b).toFixed(10)
 const multiply = (a, b) => (a * b) % 1 === 0 ? a * b : (a * b).toFixed(10)
 const divide = (a, b) => (a / b) % 1 === 0 ? a / b : (a / b).toFixed(10)
+const exponent = (a, b) => (a ^ b) % 1 === 0 ? a ** b : (a ** b).toFixed(10)
+const factorial = (a) => [...new Array(a).keys()].map(e => e + 1).reduce((a, c) => a * c, 1)
 
 
 //Todo:
